38acca6ef6c73e355e6a1494902866dd1c64a729
DESKTOP-KMC7OOG\manul <poni0205@gmail.com>
2017-04-17 00:06:58 +0900

    [] - exam10

---
+++ exam10/js/checkbox.js
@@ -0,0 +1,46 @@
+(function (win, $) {
+    'use strict';
+    win.examProject = win.examProject || {};
+    win.examProject.common = win.examProject.common || {};
+
+    var UTIL = win.examProject.common.util;
+
+    win.examProject.checkBoxUi = function (container, args) {
+        var defParams = {
+            chkBox : '.check_box',
+            iptChk : '.ipt_chk',
+            activeClass : 'chk_active'
+        };
+        this.obj = container;
+        this.opts = UTIL.def(defParams, (args || {}));
+        this.init();
+    };
+    win.examProject.checkBoxUi.prototype = {
+        init : function () {
+            this.setElements();
+            this.bindEvents();
+        },
+        setElements : function () {
+            this.checkBox = $(this.opts.chkBox);
+            this.inputCheck = this.checkBox.find(this.opts.iptChk);
+        },
+        bindEvents : function () {
+            this.inputCheck.on('change', $.proxy(this.changBox, this));
+        },
+        changBox : function (e) {
+            var target = $(e.currentTarget);
+            target.closest(this.opts.chkBox).toggleClass(this.opts.activeClass, target.prop('checked'));
+        }
+    };
+    $.fn.examProjectCheckBoxUi = function (args) {
+        var returnLengths = [];
+        for (var i = 0, max = $(this).length; i < max; i++) {
+            returnLengths.push(new win.examProject.checkBoxUi($(this).eq(i), args));
+        }
+        return returnLengths;
+    };
+
+    $(function () {
+        $('.js-check-wrap').examProjectCheckBoxUi();
+    });
+})(window, window.jQuery);
---
+++ exam10/js/select.js
@@ -0,0 +1,70 @@
+(function (win, $) {
+    'use strict';
+    win.examProject = win.examProject || {};
+    win.examProject.common = win.examProject.common || {};
+
+    var UTIL = win.examProject.common.util;
+
+    win.examProject.selectBoxUi = function (container, args) {
+        var defParams = {
+            selectBox : '.select_box',
+            selectBtn : '.select_menu',
+            selectMenu : 'ul',
+            activeClass : 'opened'
+        };
+        this.obj = container;
+        this.opts = UTIL.def(defParams, (args || {}));
+        this.init();
+    };
+    win.examProject.selectBoxUi.prototype = {
+        init : function () {
+            this.setElements();
+            this.setLayout();
+            this.bindEvents();
+        },
+        setElements : function () {
+            this.selectBox = this.obj.find(this.opts.selectBox);
+            this.selectObj = this.obj.find(this.opts.selectMenu);
+            this.selectBtn = this.obj.find(this.opts.selectBtn);
+            this.selectChild = this.selectObj.children();
+        },
+        setLayout : function () {
+            this.selectObj.hide();
+        },
+        bindEvents : function () {
+            this.selectBtn.on('click', $.proxy(this.toggleFunc, this));
+            this.selectChild.on('click', '>a', $.proxy(this.onChangeText, this));
+        },
+        toggleFunc : function (e) {
+            e.preventDefault();
+            if (!this.selectObj.is(':visible')) {
+                this.selectObj.show();
+                this.obj.on('clickoutside', $.proxy(this.outsideFunc, this));
+            } else {
+                this.selectObj.hide();
+                this.obj.off('clickoutside');
+            }
+        },
+        outsideFunc : function () {
+            this.selectBtn.triggerHandler('click');
+        },
+        onChangeText : function (e) {
+            e.preventDefault();
+            var target = $(e.currentTarget),
+                targetText = target.text();
+            this.selectBtn.text(targetText);
+            this.outsideFunc();
+        }
+    };
+    $.fn.examProjectSelectBoxUi = function (args) {
+        var returnLengths = [];
+        for (var i = 0, max = $(this).length; i < max; i++) {
+            returnLengths.push(new win.examProject.selectBoxUi($(this).eq(i), args));
+        }
+        return returnLengths;
+    };
+
+    $(function () {
+        $('.js-select-wrap').examProjectSelectBoxUi();
+    });
+})(window, window.jQuery);
